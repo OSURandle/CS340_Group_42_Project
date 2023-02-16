53891d737ef5e2d0f359cc9bd04fc68829019ed8
Joshua Randle <randlejo@oregonstate.edu>
2023-02-16 00:28:10 -0600

    html files for database

---
+++ CS340_QUERIES.sql
@@ -0,0 +1,57 @@
+-- Joshua Randle
+-- Edward G Breard
+-- Group 42
+
+-- Customer Table
+
+INSERT
+
+
+SELECT
+
+
+-- Boxes Table
+
+INSERT
+
+
+SELECT
+
+
+UPDATE
+
+
+
+-- Distributors Table
+
+INSERT
+
+
+SELECT
+
+
+DELETE
+
+
+-- Products Table
+
+INSERT
+
+
+SELECT
+
+
+-- Drop-down
+
+
+-- Distributor_Products Table
+
+INSERT
+
+SELECT
+
+-- Purchases Table
+
+INSERT
+
+SELECT
