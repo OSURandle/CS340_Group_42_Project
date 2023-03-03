e8e9604b3522ad1babc3a5f0d429b2cc351b8ea5
GarrettBreard <100252661+GarrettBreard@users.noreply.github.com>
2023-03-02 22:17:36 -0600

    Added Distributors Update, Read and Delete
    Referenced 
    https://github.com/osu-cs340-ecampus/flask-starter-app/blob/master/bsg_people_app/app.py
    and
    https://edstem.org/us/courses/32532/discussion/2701535

--- Webpage/app.py
+++ Webpage/app.py
@@ -7,25 +7,116 @@ import os
 
 app = Flask(__name__)
 
+# database connection
+# Template:
+# app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
+# app.config["MYSQL_USER"] = "cs340_OSUusername"
+# app.config["MYSQL_PASSWORD"] = "XXXX" | last 4 digits of OSU id
+# app.config["MYSQL_DB"] = "cs340_OSUusername"
+# app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
 app.config['MYSQL_USER'] = 'cs340_randlejo'
 app.config['MYSQL_PASSWORD'] = '5633' #last 4 of onid
-app.config['MYSQL_DB'] = 'cs340_RandleJo' ###check this please****
+app.config['MYSQL_DB'] = 'cs340_randlejo' ###check this please****
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+
+mysql = MySQL(app)
 
 # Routes 
 
-@app.route('/')
+@app.route('/', methods=["POST","GET"])
 def root():
     return render_template("Index.j2")
 
-@app.route("/Boxes.j2")
+@app.route("/Boxes.j2", methods=["POST","GET"])
 def boxes():
     return render_template("Boxes.j2")
 
-@app.route("/Distributors.j2")
+@app.route("/Distributors.j2", methods=["POST","GET"])
 def distributor():
-    return render_template("Distributors.j2")
+    if request.method == 'POST':
+        input_distributor_id = request.form['input-distributor-id']
+        input_distributor_name = request.form['input-distributor-name']
+
+        query = """
+                INSERT INTO distributors (distributor_id, name)
+                VALUES ('%s', '%s')""" % (input_distributor_id, input_distributor_name)
+
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        mysql.connection.commit()
+
+        cur.close()
+
+        return redirect('/distributors')
+    
+    if request.method == 'GET':
+
+        search_query = request.args
+
+        if search_query:
+            search_query = search_query['search']
+            query = """SELECT distributor_id,
+                                name
+                        FROM distributors
+                        WHERE UPPER(distributor_id) LIKE UPPER(CONCAT('%%', '%s',
+                            '%%'))
+                           OR UPPER(name) LIKE UPPER(CONCAT('%%', '%s', '%%'))
+                    """ % (search_query, search_query)
+        else:
+            query = "SELECT distributor_id, name FROM distributors ORDER BY distributor_id"
+
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        db_distributors = cur.fetchall()
+        
+        return render_template("Distributors.j2", distributors=db_distributors)
+    
+@app.route('/delete_distributors/<string:distributor_id>')
+def delete_distributor(distributor_id):
+    query = "DELETE FROM distributors WHERE distributor_id = '%s'" % (distributor_id)
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    mysql.connection.commit()
+
+    cur.close()
+
+    return redirect('/distributor')
+    
+@app.route('/update_distributors/<string:distributors_id>', methods=['POST', 'GET'])
+def update_distributors(distributor_id):
+
+    if request.method == 'GET':
+        query = """
+                SELECT distributor_id,
+                        name
+                FROM distributors WHERE distributor_id = '%s'
+                """ % (distributor_id)
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        db_distributors = cur.fetchone()
+        cur.close()
+
+        return render_template(
+            "forms/update_distributors.j2",
+            distributors=db_distributors)
+
+    if request.method == 'POST':
+        input_distributor_name = request.form['distributor-name']
+
+        query = """
+                UPDATE distributors
+                SET name = '%s'
+                WHERE distributor_id = '%s'""" % (input_distributor_name, distributor_id)
+
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        mysql.connection.commit()
+
+        cur.close()
+
+        return redirect('/distributors')
 
 @app.route("/Distributor_Products.j2")
 def dist_prod():
