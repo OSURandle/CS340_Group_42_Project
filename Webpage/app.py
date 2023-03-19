7064b155acc851856615234d470a597da97644f3
GarrettBreard <100252661+GarrettBreard@users.noreply.github.com>
2023-03-19 18:55:31 -0500

    from discord

--- Webpage/app.py
+++ Webpage/app.py
@@ -18,146 +18,127 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
 app.config['MYSQL_USER'] = 'cs340_randlejo'
 app.config['MYSQL_PASSWORD'] = '5633' #last 4 of onid
-app.config['MYSQL_DB'] = 'cs340_randlejo' ###check this please****
+app.config['MYSQL_DB'] = 'cs340_randlejo' 
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 mysql = MySQL(app)
 
 # Routes 
 
-@app.route('/', methods=["POST","GET"])
-def root():
+@app.route("/")
+def index():
     return render_template("Index.j2")
 
-@app.route("/Boxes.j2", methods=["POST","GET"])
-def boxes():
-    return render_template("Boxes.j2")
+@app.route("/Index")
+def home():
+    return render_template("Index.j2")
 
-@app.route("/Distributors.j2", methods=["POST","GET"])
-def distributor():
-    if request.method == 'POST':
-        distributor_name = request.form['distributorName']
-        distributor_email = request.form['distributorEmail']
-        distributor_phone = request.form['distributorPhone']
-        distributor_address = request.form['distributorAddress']
-        distributor_city = request.form['distributorCity']
-        distributor_state = request.form['distributorState']
-        distributor_zipcode = request.form['distributorZipcode']
-        distributor_product = request.form['distributorProduct']
-        distributor_price = request.form['distributorPrice']
+# Customer Routes
 
-        query = f"""
-                INSERT INTO Distributors (boxID, distributorName, distributorEmail, distributorPhone, distributorAddress, distributorCity, distributorState, distributorZipcode, distributorProduct, distributorPrice)
-                VALUES (1, '{distributor_name}', '{distributor_email}', '{distributor_phone}', '{distributor_address}', '{distributor_city}', '{distributor_state}', '{distributor_zipcode}', '{distributor_product}', '{distributor_price}')
-                """
+@app.route("/Customers", methods=["POST", "GET"])
+def customers():
+
+    if request.method == "POST":
+        if request.form.get("Add_Customer"):
+            customerName = request.form["customerName"]
+            customerEmail = request.form["customerEmail"]
+            customerPhone = request.form["customerPhone"]
+            customerAddress = request.form["customerAddress"]
+            customerCity = request.form["customerCity"]
+            customerState = request.form["customerState"]
+            customerZipcode = request.form["customerZipcode"]
+            query = "INSERT INTO Customers(customerName, customerEmail, customerPhone, customerAddress, customerCity, customerState, customerZipcode) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (customerName, customerEmail, customerPhone, customerAddress, customerCity, customerState, customerZipcode))
+            mysql.connection.commit()
+        return redirect("/Customers")
+    # Populates Customers table
+    if request.method == "GET":
+        query = "SELECT customerID, customerName, customerEmail, customerPhone, customerAddress, customerCity, customerState, customerZipcode FROM Customers;"
         cur = mysql.connection.cursor()
         cur.execute(query)
-        mysql.connection.commit()
-        cur.close()
+        data = cur.fetchall()
+        return render_template("Customers.j2", data=data)
 
-        return redirect('/Distributors.j2')
-    
-    if request.method == 'GET':
-        search_query = request.args.get('search')
 
-        if search_query:
-            query = f"""
-                    SELECT distributorID,
-                            distributorName,
-                            distributorEmail,
-                            distributorPhone,
-                            distributorAddress,
-                            distributorCity,
-                            distributorState,
-                            distributorZipcode,
-                            distributorProduct,
-                            distributorPrice
-                    FROM Distributors
-                    WHERE UPPER(distributorName) LIKE UPPER('%{search_query}%')
-                    """
-        else:
-            query = """
-                    SELECT distributorID,
-                            distributorName,
-                            distributorEmail,
-                            distributorPhone,
-                            distributorAddress,
-                            distributorCity,
-                            distributorState,
-                            distributorZipcode,
-                            distributorProduct,
-                            distributorPrice
-                    FROM Distributors
-                    ORDER BY distributorID
-                    """
-
-        cur = mysql.connection.cursor()
-        cur.execute(query)
-        db_distributors = cur.fetchall()
-        cur.close()
-
-        return render_template("Distributors.j2", distributors=db_distributors)
-    
-@app.route('/delete_distributors/<string:distributor_id>')
-def delete_distributor(distributor_id):
-    query = f"DELETE FROM Distributors WHERE distributorID = '{distributor_id}'"
+@app.route("/delete_customer/<int:customerID>")
+def delete_customer(customerID):
+    # mySQL query to delete the planet with our passed id
+    query = "DELETE FROM Customers WHERE customerID = %s;"
     cur = mysql.connection.cursor()
-    cur.execute(query)
+    cur.execute(query, (customerID,))
     mysql.connection.commit()
-    cur.close()
 
-    return redirect('/Distributors.j2')
+    # redirect back to customers page
+    return redirect("/Customers")
+
+@app.route("/edit_Customers/<int:customerID>", methods=["POST", "GET"])
+def edit_Customers(customerID):
+    if request.method == "GET":
+        query = "SELECT * FROM Customers WHERE customerID = %s" % (customerID)
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+        return render_template("edit_Customers.j2", data=data)
     
-@app.route('/update_distributors/<int:distributor_id>', methods=['POST', 'GET'])
-def update_distributors(distributor_id):
-    if request.method == 'GET':
+    if request.method == "POST":
+        if request.form.get("Edit_Customer"):
+            
+            customerName = request.form["customerName"]
+            customerEmail = request.form["customerEmail"]
+            customerPhone = request.form["customerPhone"]
+            customerAddress = request.form["customerAddress"]
+            customerCity = request.form["customerCity"]
+            customerState = request.form["customerState"]
+            customerZipcode = request.form["customerZipcode"]
+            query = "UPDATE Customers SET Customers.customerName = %s, Customers.customerEmail = %s, Customers.customerPhone = %s, Customers.customerAddress = %s, Customers.customerCity = %s, Customers.customerState = %s, Customers.customerZipcode = %s WHERE Customers.customerID = %s"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (customerName, customerEmail, customerPhone, customerAddress, customerCity, customerState, customerZipcode, customerID))
+            mysql.connection.commit()
+            return redirect("/Customers")
+
+# Boxes routes
+
+@app.route("/Boxes", methods=["POST", "GET"])
+def boxes():
+
+    if request.method == "POST":
+        if request.form.get("Add_Box"):
+            boxType = request.form["boxType"]
+            boxPrice = request.form["boxPrice"]
+            query = "INSERT INTO Boxes(boxType, boxPrice) VALUES (%s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (boxType,boxPrice))
+            mysql.connection.commit()
+        return redirect("/Boxes")
+    # Populates Boxes table
+    if request.method == "GET":
+        query = "SELECT boxID, boxType, boxPrice FROM Boxes;"
         cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM Distributors WHERE distributorID = %s', [distributor_id])
-        distributor = cur.fetchone()
-        cur.close()
-        return render_template('update_distributors.j2', distributor=distributor)
+        cur.execute(query)
+        data = cur.fetchall()
+        return render_template("Boxes.j2", data=data)
 
-    elif request.method == 'POST':
-        name = request.form['distributorName']
-        email = request.form['distributorEmail']
-        phone = request.form['distributorPhone']
-        address = request.form['distributorAddress']
-        city = request.form['distributorCity']
-        state = request.form['distributorState']
-        zipcode = request.form['distributorZipcode']
-        product = request.form['distributorProduct']
-        price = request.form['distributorPrice']
 
-        cur = mysql.connection.cursor()
-        cur.execute(
-            """
-            UPDATE Distributors
-            SET distributorName = %s, distributorEmail = %s, distributorPhone = %s,
-                distributorAddress = %s, distributorCity = %s, distributorState = %s,
-                distributorZipcode = %s, distributorProduct = %s, distributorPrice = %s
-            WHERE distributorID = %s
-            """,
-            (name, email, phone, address, city, state, zipcode, product, price, distributor_id)
-        )
-        mysql.connection.commit()
-        cur.close()
+@app.route("/delete_box/<int:boxID>")
+def delete_box(boxID):
+    # mySQL query to delete the planet with our passed id
+    query = "DELETE FROM Boxes WHERE boxID = %s;"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (boxID,))
+    mysql.connection.commit()
 
-        return redirect('/Distributors.j2')
+    # redirect back to Boxes page
+    return redirect("/Boxes")
 
-@app.route("/Distributor_Products.j2")
-def dist_prod():
-    return render_template("Distributor_Products.j2")
+# Distributor routes
 
-@app.route("/Products.j2")
-def products():
-    return render_template("Products.j2")
+# Distributor_Boxes routes
 
-@app.route("/Purchases.j2")
-def purchases():
-    return render_template("Purchases.j2")
+# Purchase routes
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 1989)) 
+    port = int(os.environ.get('PORT', 1919)) 
  
-    app.run(port=port, debug=True) 
+    app.run(port=port, debug=True)
